a9fbf702a8609a5433b6f3c93c052b2beee66e76
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2025-04-09 10:05:36 +0530

    Update Assignement1.cpp

--- Assignement1.cpp
+++ Assignement1.cpp
@@ -1,48 +1,37 @@
-#include<iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 #include<queue>
 using namespace std;
 class Graph
 {
-    int v;
+    int n;
     int** graph;
     public:
     Graph()
     {
-        cout<<"\nEnter no of vertices:";
-        cin>>v;
-        graph=new int*[v];
-        for(int i=0;i<v;i++)
+        cout<<"\nEnter no of nodes:";
+        cin>>n;
+        graph=new int*[n];
+        for(int i=0;i<n;i++)
         {
-            graph[i]=new int[v];
-            for(int j=0;j<v;j++)
-            {
-                graph[i][j]=0;
-            }
-        }
-
-    }
-    void scanbyEdge()
-    {
-        int u,v;
-        while(1)
-        {
-            cout<<"\nEnter u:";
-            cin>>u;
-            if(u==-1)
-            {
-                return;
-            }
-            cout<<"\nEnter v:";
-            cin>>v;
-            graph[u][v]=1;
-            graph[v][u]=1;
+            graph[i]=new int[n];
         }
     }
-    void printmatrix()
+    void readGraph()
     {
-        for(int i=0;i<v;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<v;j++)
+            for(int j=0;j<n;j++)
+            {
+                cin>>graph[i][j];
+            }
+        }
+    }
+     void showGraph()
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
             {
                 cout<<graph[i][j]<<" ";
             }
@@ -51,67 +40,73 @@ class Graph
     }
     void dfs()
     {
-        int* visited=new int[v];
-        for(int i=0;i<v;i++)
+        int* visited=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            visited[i]=0;
+        }
+        int start;
+        cout<<"\nEnter strating vertex:";
+        cin>>start;
+        cout<<"\nDFS Traversal";
+        dfs_rec(visited,start);
+    }
+    void dfs_rec(int* visited,int v)
+    {
+        cout<<"\n"<<v<<" is visited";
+        visited[v]=1;
+        for(int i=0;i<n;i++)
+        {
+            if(graph[v][i]!=0 && visited[i]==0)
+            {
+                dfs_rec(visited,i);
+            }
+        }
+    }
+    void bfs()
+    {
+        int* visited=new int[n];
+        for(int i=0;i<n;i++)
         {
             visited[i]=0;
         }
         int start;
         cout<<"\nEnter start vertex:";
         cin>>start;
-        dfs_rec(start,visited);
+        queue<int>q;
+        q.push(start);
+        cout<<"\nBFS Traversal:" ;
+        bfs_rec(q,visited);
     }
-    void dfs_rec(int start,int* visited)
+    void bfs_rec(queue<int>&q,int* visited)
     {
-        cout<<"\n"<<start<<" is visited";
-        visited[start]=1;
-        for(int i=0;i<v;i++)
+        if(q.empty())
         {
-            if(graph[start][i]!=0 && !visited[i])
-            {
-                dfs_rec(i,visited);
-            }
+            return;
         }
-    }
-    void bfs_helper(int* visited,queue<int>&q){
-            if(q.empty())
-                return;
-    
-            int src = q.front();
+        int v=q.front();
+        if(visited[v]==0)
+        {
+            cout<<"\n"<<v<<" is visted ";
+            visited[v]=1;
             q.pop();
-    
-            if(!visited[src]){
-                cout << src << " ";
-                visited[src] =1;
-            }
-    
-            for(int i=0;i<v;i++){
-                if(graph[src][i]!=0 && !visited[i]){
+            for(int i=0;i<n;i++)
+            {
+                if(graph[v][i]!=0 && visited[i]==0)
+                {
                     q.push(i);
                 }
             }
-            bfs_helper(visited, q);
+            bfs_rec(q,visited);  
         }
-        void bfs_rec(int src){
-           int* visited=new int[v];
-           for(int i=0;i<v;i++)
-           {
-            visited[i]=0;
-           }
-            queue<int> q;
-            q.push(src);
-    
-            bfs_helper(visited, q);
-        }
+    }
 };
-int main()
+int main() 
 {
     Graph g1;
-    g1.scanbyEdge();
-    g1.printmatrix();
+    g1.readGraph();
+    g1.showGraph();
     g1.dfs();
-    int source;
-    cout<<"\nEnter source:";
-    cin>>source;
-    g1.bfs_rec(source);
+    g1.bfs();
+    return 0;
 }
