c531818a7da3eb8de813d9efb33ee011d69f9496
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2025-05-09 20:09:58 +0530

    Create rules.py

---
+++ problem16-17/rules.py
@@ -0,0 +1,8 @@
+rules={
+    ("hello","hi","hey"):["Hello dear!Welcome to Shiv9 Restaurant!!How can I serve you today?","Hey !What's up? "],
+    ("bye","by","quit","exit"):["Bye!Nice To Meet you","See you again!","Thanks for visiting us!"],
+    ("menu","items","list","item"):["Menu consist of Idli,Medu Vada,Pav Bhaji,Kadhi Vada","Listed items are present:\n1 Chole Puri\n2 Dalcha\n3 Anda Curry\n 4 Chicken"],
+    ("order","buy","take"):["Yes put order Our staff will serve you!","Yes,you can buy any no of items","Ready to take your order"],
+    ("payment","pay","cash","upi","bill"):["you can pay by any means","You can pay either cash or can do online payment"],
+    ("ok","done"):["Yes! off course "]
+}
