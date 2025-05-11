7763449a3c1bd4779a1c954a26e3d758a06b2153
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2025-05-11 18:50:12 +0530

    Update Chatbot.py

--- problem16-17/Chatbot.py
+++ problem16-17/Chatbot.py
@@ -1,24 +1,22 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+import nltk
+from rules import rules;
+from nltk.tokenize import word_tokenize;
 from nltk.stem import WordNetLemmatizer
-from rules import rules
+from nltk.corpus import stopwords
 import random
-def query_process(usr_input):
-    usr_input=usr_input.lower()
-    tokens=word_tokenize(usr_input)
+def process(str):
+    str=str.lower()
+    tokens=word_tokenize(str)
+    print("Tokens after word tokenize: ",tokens)
     stop_words=set(stopwords.words('english'))
     tokens=[word for word in tokens if word not in stop_words]
+    print("Tokens after removal of stopwords",tokens)
     lemmatizer=WordNetLemmatizer()
-    tokens=[lemmatizer.lemmatize(token) for token in tokens]
-    for keywords,responses in rules.items():
-        for keyword in tokens:
-            if(keyword in keywords):
-                return random.choice(responses)
-    return random.choice(["Can you rephrase?","Sorry!","Unable to answer!"])
-def main():
-    while(True):
-        user_input=input("You >>")
-        res=query_process(user_input)
-        print("Bot >> "+res)
-if __name__=="__main__":
-    main()
+    tokens=[lemmatizer.lemmatize(word) for word in tokens]
+    print("Tokens after lemmatization",tokens)
+    for keys,values in rules.items():
+        for key in keys:
+            if key in tokens:
+                return random.choice(values)
+    return random.choice(["can you rephrase?","Sorry,unable to give answer"])
+    
