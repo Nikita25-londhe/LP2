ffaa38339e953d60b122925b016486f3223973cb
Nikita Rajendra Londhe <146791442+Nikita25-londhe@users.noreply.github.com>
2025-05-11 18:49:52 +0530

    Update rules.py

--- problem16-17/rules.py
+++ problem16-17/rules.py
@@ -1,8 +1,82 @@
-rules={
-    ("hello","hi","hey"):["Hello dear!Welcome to Shiv9 Restaurant!!How can I serve you today?","Hey !What's up? "],
-    ("bye","by","quit","exit"):["Bye!Nice To Meet you","See you again!","Thanks for visiting us!"],
-    ("menu","items","list","item"):["Menu consist of Idli,Medu Vada,Pav Bhaji,Kadhi Vada","Listed items are present:\n1 Chole Puri\n2 Dalcha\n3 Anda Curry\n 4 Chicken"],
-    ("order","buy","take"):["Yes put order Our staff will serve you!","Yes,you can buy any no of items","Ready to take your order"],
-    ("payment","pay","cash","upi","bill"):["you can pay by any means","You can pay either cash or can do online payment"],
-    ("ok","done"):["Yes! off course "]
+rules = {
+    ("hello","hi","hii","hey"):
+    [
+        "Hello ! I am chatbot of Shiv9 Restraurant! How can I help you today?",
+        "Hey! What's up?"
+    ],
+    ("menu","items","list","item"):
+    [
+        "Menu consists of Idli, Medu Vada, Pav Bhaji, Kadhi Vada",
+        "Listed items are:\n1.Chole\n2.Dal Chawal\n3.Paneer Masala\n4.Chicken"
+    ],
+    ("order","buy","take"):
+    [
+        "Yes, please place your order. Our staff will serve you!",
+        "Yes, you can buy any number of items",
+        "Ready to take your order."
+    ],
+    ("payment","pay","bill","cash","upi"):
+    [
+        "You can pay by any means",
+        "You can pay either by cash or do online payment."
+    ],
+    ("ok","done"):
+    [
+        "Yes! Of course"
+    ],
+    ("time","timing","timings","hours","close","open"):
+    [
+        "We are oprn from 9AM to 11PM every day",
+        "Opening hours are 9AM to 11PM"
+    ],
+    ("where","location","address","place"):
+    [
+        "We are located at MG Road, Pune",
+        "You can find us at Shivaji Nagar near City Center."
+    ],
+    ("reserve","booking","table","book","reservation"):
+    [
+        "Sure, you can reserve a table by calling our front dest",
+        "Table booking is available from 10 AM to 10 PM"
+    ],
+    ("thank", "thanks", "thankyou"): 
+    [
+        "You're welcome!",
+        "Happy to help you!"
+    ],
+
+    ("bad", "complaint", "not good", "worst", "feedback"): 
+    [
+        "We are sorry to hear that. Please share your feedback with our manager.",
+        "Apologies! We'll try to improve your experience next time."
+    ],
+
+    ("help", "support", "assist", "problem","information","know"): 
+    [
+        "Sure, how can I assist you?",
+        "I'm here to help! Tell me your query."
+    ],
+
+    ("again", "repeat", "same order"): 
+    [
+        "Do you want to repeat your last order?",
+        "Sure! Repeating your previous order."
+    ],
+
+    ("offer", "discount", "deal", "coupon"): 
+    [
+        "Today's offer: 10% off on orders above ₹500.",
+        "We have a weekend special: Buy 1 Get 1 on Pav Bhaji!"
+    ],
+
+    ("contact", "call", "phone", "number"): 
+    [
+        "You can call us at +91-9876543210.",
+        "Our contact number is +91-9876543210."
+    ],
+    ("nice","good","fantastic"):
+    [
+        "Thanks!",
+        "Thank you, How can I help further?"
+    ]
 }
